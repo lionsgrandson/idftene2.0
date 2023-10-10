ff97c3597a625bf3f5b08b7e1e25f4663895d847
lionsgranson <mosheschwartzberg@gmail.com>
2023-10-10 14:54:27 +0300

    changed the name id

--- php/contact.php
+++ php/contact.php
@@ -1,9 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    $name = $_POST["myname"];
+    $email = $_POST["myemail"];
+    $message = $_POST["mymessage"];
 
     // Email configuration
     $to = "logitechidfdev@gmail.com";
@@ -13,13 +13,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Send email
     $mail_success = mail($to, $subject, $message_body, $headers);
-    header('Location: https://flourishing-toffee-be2e06.netlify.app');
-    if ($mail_success) {
-        echo "Thank you for your message. We will get back to you soon.";
-    } else {
-        echo "Oops! Something went wrong. Please try again later.";
-    }
-} else {
-    // Handle the case where the form was not submitted via POST
-    echo "Form not submitted.";
+    header('Location: "index.html"');
 }
