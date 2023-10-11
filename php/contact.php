1b6d31014bc933fa426ea928553945afca6eed84
lionsgranson <mosheschwartzberg@gmail.com>
2023-10-11 10:35:29 +0300

    copied the contact from ms3.0

--- php/contact.php
+++ php/contact.php
@@ -1,19 +1,37 @@
 <?php
-echo "This is a test";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
 
-    // Email configuration
-    $to = "logitechidfdev@gmail.com";
-    $subject = "Contact Form Submission";
-    $headers = "From: $email";
-    $message_body = "Name: $name\nEmail: $email\nMessage:\n$message";
+// check if form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    // Send email
-    $mail_success = mail($to, $subject, $message_body, $headers);
+    // get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $from = 'email@mosheschwartzberg.com';
+
+    // validate form data
+    if (empty($name) || empty($email) || empty($phone) || empty($message)) {
+        // some form data is missing
+        $error = 'Please fill out all required fields.';
+    } else {
+        // form data is valid
+        // send email
+        $to = 'moshe@mosheschwartzberg.com';
+        $subject = 'New Message from Contact Form from ' . $name;
+        $body = "Name: $name\nEmail: $email\nPhone: $phone\n\n$message";
+        $headers = "From: $from\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        if (mail($to, $subject, $body, $headers)) {
+            // email was sent
+            $msg = 'Thank you for your message! We will get back to you as soon as possible.';
+            // echo $msg;
+            header("Location: /index.html");
+        } else {
+            // email was not sent
+            $error = 'There was an error sending your message. Please try again later.';
+            // echo $error;
+            header("Location: /index.html");
+        }
+    }
 }
-header('Location: https://idftene.com');
-echo "This is a test";
